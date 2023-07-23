753c853612b6b98207faec3b9d149bdcb62fca0b
Vossik <vosik.m@seznam.cz>
2023-07-23 15:34:07 +0200

    added missing commodities attributes

--- src/FedexRest/Services/Ship/Entity/Commodities.php
+++ src/FedexRest/Services/Ship/Entity/Commodities.php
@@ -7,13 +7,24 @@ use FedexRest\Entity\Weight;
 
 class Commodities
 {
-    public string $description = '';
-    public ?Value $customsValue = null;
-    public ?string $countryOfManufacture = null;
     public ?Value $unitPrice = null;
-    public ?Weight $weight = null;
     public $additionalMeasures = null;
     public ?int $numberOfPieces = null;
+    public ?int $quantity = null;
+    public ?string $quantityUnits = null;
+    public ?Value $customsValue = null;
+    public ?string $countryOfManufacture = null;
+    public ?string $cIMarksAndNumbers = null;
+    public ?string $harmonizedCode = null;
+    public string $description = '';
+    public ?string $name = null;
+    public ?Weight $weight = null;
+    public ?string $exportLicenseNumber = null;
+    public ?string $exportLicenseExpirationDate = null;
+    public ?string $partNumber = null;
+    public ?string $purpose = null;
+
+    // TODO: usmcaDetail
 
     public function setDescription(string $description) : Commodities
     {
@@ -64,6 +75,69 @@ class Commodities
         return $this;
     }
 
+    public function setQuantity(int $quantity) : Commodities
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function setQuantityUnits(string $quantityUnits) : Commodities
+    {
+        $this->quantityUnits = $quantityUnits;
+
+        return $this;
+    }
+
+    public function setCIMarksAndNumbers(string $cIMarksAndNumbers) : Commodities
+    {
+        $this->cIMarksAndNumbers = $cIMarksAndNumbers;
+
+        return $this;
+    }
+
+    public function setHarmonizedCode(string $harmonizedCode) : Commodities
+    {
+        $this->harmonizedCode = $harmonizedCode;
+
+        return $this;
+    }
+
+    public function setPurpose(string $purpose) : Commodities
+    {
+        $this->purpose = $purpose;
+
+        return $this;
+    }
+
+    public function setPartNumber(string $partNumber) : Commodities
+    {
+        $this->partNumber = $partNumber;
+
+        return $this;
+    }
+
+    public function setExportLicenseExpirationDate(string $exportLicenseExpirationDate) : Commodities
+    {
+        $this->exportLicenseExpirationDate = $exportLicenseExpirationDate;
+
+        return $this;
+    }
+
+    public function setExportLicenseNumber(string $exportLicenseNumber) : Commodities
+    {
+        $this->exportLicenseNumber = $exportLicenseNumber;
+
+        return $this;
+    }
+
+    public function setName(string $name) : Commodities
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
     public function prepare(): array
     {
         $data = [];
@@ -92,6 +166,42 @@ class Commodities
 
         if ($this->numberOfPieces !== null) {
             $data['numberOfPieces'] = $this->numberOfPieces;
+        }
+
+        if ($this->quantity !== null) {
+            $data['quantity'] = $this->quantity;
+        }
+
+        if ($this->quantityUnits !== null) {
+            $data['quantityUnits'] = $this->quantityUnits;
+        }
+
+        if ($this->cIMarksAndNumbers !== null) {
+            $data['cIMarksAndNumbers'] = $this->cIMarksAndNumbers;
+        }
+
+        if ($this->harmonizedCode !== null) {
+            $data['harmonizedCode'] = $this->harmonizedCode;
+        }
+
+        if ($this->purpose !== null) {
+            $data['purpose'] = $this->purpose;
+        }
+
+        if ($this->partNumber !== null) {
+            $data['partNumber'] = $this->partNumber;
+        }
+
+        if ($this->exportLicenseExpirationDate !== null) {
+            $data['exportLicenseExpirationDate'] = $this->exportLicenseExpirationDate;
+        }
+
+        if ($this->exportLicenseNumber !== null) {
+            $data['exportLicenseNumber'] = $this->exportLicenseNumber;
+        }
+
+        if ($this->name !== null) {
+            $data['name'] = $this->name;
         }
 
         return $data;
